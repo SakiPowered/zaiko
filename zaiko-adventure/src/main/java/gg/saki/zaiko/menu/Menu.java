15045aa54aa4159701eed35b69f653d698606df7
DylanDeNewb <strawdylan1@gmail.com>
2023-01-25 13:36:05 +0000

    fix adventure menu

--- zaiko-adventure/src/main/java/gg/saki/zaiko/menu/Menu.java
+++ zaiko-adventure/src/main/java/gg/saki/zaiko/menu/Menu.java
@@ -5,10 +5,6 @@ import net.kyori.adventure.text.Component;
 
 public abstract class Menu extends BaseMenu<Component> {
 
-    public Menu(String stringIdentifier) {
-        super(stringIdentifier);
-    }
-
     @Override
     public Slot<Component> createSlot(BaseMenu<Component> menu, int index, boolean locked) {
         Slot<Component> slot = new ComponentSlot(menu, index);
